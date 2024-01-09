a0c760bd4281f3efac3bb5062c2b52e126e60921
yenteho-felix <yenteho@gmail.com>
2024-01-09 13:38:17 -0800

    Finished background, ground and bird spawn

--- CS50-Game/bird/v1.0/Bird.lua
+++ CS50-Game/bird/v1.0/Bird.lua
@@ -7,3 +7,20 @@
 
     Author: Felix Ho
 ]]
+
+Bird = Class{}
+
+function Bird:init()
+    -- set bird image, widht and height
+    self.image = love.graphics.newImage('image/bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    -- set bird dropping location
+    self.x = VIRTUAL_WIDTH / 2 - self.width / 2
+    self.y = VIRTUAL_HEIGHT / 2 - self.height / 2
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
--- CS50-Game/bird/v1.0/main.lua
+++ CS50-Game/bird/v1.0/main.lua
@@ -34,7 +34,31 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+-- background image
+local background = love.graphics.newImage('image/background.png')
+local backgroundScroll = 0
+local BACKGROUND_SCROLL_SPEED = 30
+local BACKGROUND_LOOPING_POINT = 413
+
+-- ground image. ground should move faster than background
+local ground = love.graphics.newImage('image/ground.png')
+local groundScroll = 0
+local GROUND_SCROLL_SPEED = BACKGROUND_SCROLL_SPEED * 2
+local GROUND_LOOPING_POINT = ground:getWidth() / 2
+
+-- bird sprite
+local bird = Bird()
+
+
 function love.load()
+    -- sceme title, resolution
+    love.window.setTitle('Fifty Bird')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true,
+    })
 end
 
 function love.resize(w, h)
@@ -42,10 +66,30 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.update(dt)
+    -- scroll background by present speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    -- scroll ground by present speed * dt, looping back to 0 after the looping point
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 end
 
 function love.draw()
+    Push:start()
+
+    -- draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    -- draw the ground on top of background, toward the bottom of the screen at the negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - ground:getHeight())
+
+    -- draw the bird sprite
+    bird:render()
+
+    Push:finish()
 end
