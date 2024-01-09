cb70ba09541f734c70d9396904003084f0d66244
yenteho-felix <yenteho@gmail.com>
2024-01-09 10:01:42 -0800

    Initial

---
+++ CS50-Game/bird/v1.0/Bird.lua
@@ -0,0 +1,9 @@
+--[[
+    Bird Class
+
+    The Bird is what we control in the game via clicking or the space bar; whenever we press either,
+    the bird will flap and go up a little bit, where it will then be affected by gravity. If the bird hits
+    the ground or a pipe, the game is over.
+
+    Author: Felix Ho
+]]
---
+++ CS50-Game/bird/v1.0/main.lua
@@ -0,0 +1,23 @@
+--[[
+    GD50
+    Flappy Bird Remake
+    
+    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
+    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
+    the screen, making the player's bird avatar flap its wings and move upwards slightly. 
+    A variant of popular games like "Helicopter Game" that floated around the internet
+    for years prior. Illustrates some of the most basic procedural generation of game
+    levels possible as by having pipes stick out of the ground by varying amounts, acting
+    as an infinitely generated obstacle course for the player.
+
+    Lecture's Scope:
+    -- Images (Sprites)
+    -- Infinite Scrolling
+    -- Games Are Illusions
+    -- Procedural Generation
+    -- State Machines
+    -- Mouse Input
+
+    Author: Felix Ho
+]]
+
