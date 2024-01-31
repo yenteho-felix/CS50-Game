afdb93aa6baddfc1726cb0a8094af1d5a66e5eea
yenteho-felix <yenteho@gmail.com>
2024-01-30 19:19:57 -0800

    Add tile cursor and tile swap function 1. tile cursor 2. highlight tile when selected 3. swap neighboring tiles

--- CS50-Game/match3/v1.0/src/states/PlayState.lua
+++ CS50-Game/match3/v1.0/src/states/PlayState.lua
@@ -15,6 +15,17 @@ function PlayState:init()
     self.timer = 20
     self.paused = false
 
+    -- position of the board
+    self.boardX = VIRTUAL_WIDTH - 8 * 32 - 16
+    self.boardY = 16
+
+    -- position in the grid which we're highlighting
+    self.currentTileX = 0
+    self.currentTileY = 0
+
+    -- tile object highlighted to be swap
+    self.highlightedTile = nil
+
     self:timerCountDown()
 end
 
@@ -22,7 +33,7 @@ function PlayState:enter(params)
     self.level = params.level
 
     -- Continue the board from previous level or generate new
-    self.board = params.board or Board(VIRTUAL_WIDTH - 8 * 32 - 16, 16)
+    self.board = params.board or Board(self.boardX, self.boardY)
 
     -- Continue the score from previous level or start with to 0
     self.score = params.score or 0
@@ -57,10 +68,106 @@ function PlayState:render()
 
     -- pause
     self:renderPaused()
+
+    -- cursor movement
+    self:renderCursor()
+
+    -- highted tile
+    self:renderTileSelected()
 end
 
 -- Play game
 function PlayState:playGame()
+    -- cursor movement
+    self:moveCursor()
+
+    -- tile selected
+    self:tileSelected()
+end
+
+-- User move cursor up/down/left/right to move between tiles
+-- If a tile being selected previously, restrict the movement to its neighboring only
+function PlayState:moveCursor()
+    -- Define the boundaries of the game board
+    local boardMinX, boardMinY = 0, 0
+    local boardMaxX, boardMaxY = 7 * 32, 7 * 32
+
+    -- Function to update cursor position and play sound
+    local function updatePositionAndPlaySound(newX, newY)
+        if self:isValid(newX, newY) then
+            self.currentTileX, self.currentTileY = newX, newY
+            gSounds['select']:play()
+        else
+            gSounds['error']:play()
+        end
+    end
+
+    -- Define direction offsets for each key
+    local dir = {up = {0, -32}, down = {0, 32}, left = {-32, 0}, right = {32, 0}}
+
+    -- When any movement key is pressed, calculate new position based on the direction offset
+    for key, offset in pairs(dir) do
+        if love.keyboard.wasPressed(key) then
+            local newX = math.max(boardMinX, math.min(boardMaxX, self.currentTileX + offset[1]))
+            local newY = math.max(boardMinY, math.min(boardMaxY, self.currentTileY + offset[2]))
+            updatePositionAndPlaySound(newX, newY)
+            -- Exit the loop after the first key press is detected
+            break
+        end
+    end
+end
+
+-- Check if this movement if valid or not; Always valid when tile not highlighted
+function PlayState:isValid(nextX, nextY)
+    if not self.highlightedTile then
+        return true
+    else
+        return math.abs(nextX - self.highlightedTile.x) + math.abs(nextY - self.highlightedTile.y) <= 32
+    end
+end
+
+-- User press 'enter' to select tile; Select neighboring tile to do tile swap
+function PlayState:tileSelected()
+    if love.keyboard.wasPressed('return') or love.keyboard.wasPressed('enter') then
+        local row = self.currentTileY / 32 + 1      -- 1 index
+        local col = self.currentTileX / 32 + 1      -- 1 index
+
+        -- If nothing is highlighted, highlight current tile
+        if not self.highlightedTile then
+            self.highlightedTile = self.board.tiles[row][col]
+        
+        -- If a tile is highlighted and current tile is the highted one, remove highlight
+        elseif self.highlightedTile == self.board.tiles[row][col] then
+            self.highlightedTile = nil
+
+        -- If a tile is highlighted and current tile is not the highted one, do swap
+        else
+            self:swapTiles(self.highlightedTile, self.board.tiles[row][col])
+            -- self:calculateMatches()
+        end
+    end
+end
+
+-- Swap the location of tile1 and tile2
+function PlayState:swapTiles(tile1, tile2)
+
+    -- Create a temporary tile to store the properties of tile1
+    local tempTile = Tile(tile1.row, tile1.col)
+    tempTile.row, tempTile.col = tile1.row, tile1.col
+    tempTile.x, tempTile.y = tile1.x, tile1.y
+
+    -- Update tile1's properties with tile2's properties
+    tile1.row, tile1.col = tile2.row, tile2.col
+    self.board.tiles[tile1.row][tile1.col] = tile1
+    Timer.tween(0.1, {[tile1] = {x = tile2.x, y = tile2.y}})
+
+    -- Update tile2's properties with the temporary tile's properties
+    tile2.row, tile2.col = tempTile.row, tempTile.col
+    self.board.tiles[tile2.row][tile2.col] = tile2
+    Timer.tween(0.1, {[tile2] = {x = tempTile.x, y = tempTile.y}})
+
+    -- un-highlight after tile swap
+    self.highlightedTile = nil
 end
 
 -- Enter to game over
@@ -134,5 +241,29 @@ function PlayState:renderPaused()
         love.graphics.setFont(gFonts['large'])
         love.graphics.setColor(1,1,1,1)
         love.graphics.printf('Game Paused', 0, 120, VIRTUAL_WIDTH, 'center')
+    end
+end
+
+-- Render cursor on the board; A rectangle shape follows user movement 
+-- to indicate which tile user is at in the game play
+function PlayState:renderCursor()
+    local recX = self.boardX + self.currentTileX
+    local recY = self.boardY + self.currentTileY
+
+    love.graphics.setColor(217/255, 87/255, 99/255, 1)
+    love.graphics.setLineWidth(4)
+    love.graphics.rectangle('line', recX, recY, 32, 32, 4)
+end
+
+-- Render tiles which are selected; The selected tile is brighter
+function PlayState:renderTileSelected()
+    if self.highlightedTile then
+        local recX = self.boardX + self.highlightedTile.x
+        local recY = self.boardY + self.highlightedTile.y
+
+        love.graphics.setBlendMode('add')
+        love.graphics.setColor(1,1,1, 96/255)
+        love.graphics.rectangle('fill', recX, recY, 32, 32, 4)
+        love.graphics.setBlendMode('alpha')
     end
 end
